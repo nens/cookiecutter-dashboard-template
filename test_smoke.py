a920b2f1719a26b18ff8d54235ef192f8e03f6cd
Reinout van Rees <reinout@vanrees.org>
2026-02-17 16:50:41 +0100

    Added dashboard

--- test_smoke.py
+++ test_smoke.py
@@ -1,7 +1,7 @@
 import sys
 from contextlib import chdir
 from pathlib import Path
-from subprocess import run
+from subprocess import run, CalledProcessError
 
 import pytest
 from cookiecutter.main import cookiecutter
@@ -31,11 +31,11 @@ def test_function_prefix(tmp_path: Path):
         },
         no_input=True,
     )
-    generated_flows_py = tmp_path / "nens-customer-dashboard/src/flows.py"
+    generated_dashboard_py = tmp_path / "nens-customer-dashboard/dashboard.py"
     # We prefix everything with our project name, but want to omit the '-dashboard'
     # part.
-    print(generated_flows_py.read_text())  # For easier debugging
-    assert "def nens_customer_flow" in generated_flows_py.read_text()
+    print(generated_dashboard_py.read_text())  # For easier debugging
+    assert 'page_title="nens-customer-dashboard"' in generated_dashboard_py.read_text()
 
 
 def test_generated_project_ruff(tmp_path: Path):
@@ -63,7 +63,12 @@ def test_generated_project_precommit(tmp_path: Path):
     with chdir(tmp_path / "my-example-dashboard"):
         run(["git", "init"], check=True)
         run(["git", "add", "-A"], check=True)
-        run([sys.executable, "-m", "pre_commit", "run", "--all"], check=True)
+        try:
+            run([sys.executable, "-m", "pre_commit", "run", "--all"], check=True)
+        except CalledProcessError:
+            print("pre-commit error, here is the 'git diff':")
+            run(["git", "diff"])
+            assert "format" == "ok"  # :-)
 
 
 def test_generated_project_install(tmp_path: Path):
--- {{ cookiecutter.project_name }}/dashboard.py
+++ {{ cookiecutter.project_name }}/dashboard.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
+second_page = st.Page("second_page.py", title="Tweede pagina")
+
+home_page = st.navigation([second_page])
+
+st.set_page_config(page_title="{{ cookiecutter.project_name }}")
 st.write("Reinout is geweldig!")
+home_page.run()
