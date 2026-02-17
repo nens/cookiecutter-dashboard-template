5948f5b0d8357f2e6213756e87f1d0dbbabaf66a
Reinout van Rees <reinout@vanrees.org>
2026-02-17 11:27:03 +0100

    prefect/dashboard fixes

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,7 +3,6 @@
 import sys
 
 PROJECT_NAME = "{{ cookiecutter.project_name }}"
-PREFECT_VERSION = "{{ cookiecutter.prefect_version }}"
 
 
 def validate_project_name(project_name: str = PROJECT_NAME):
@@ -14,18 +13,10 @@ def validate_project_name(project_name: str = PROJECT_NAME):
     if "_" in project_name:
         print("ERROR: Convention - project_name should use - instead of underscores")
         sys.exit(1)
-    if not project_name.startswith("prefect-"):
-        print("ERROR: Convention - project_name should start with 'prefect-'")
-        sys.exit(1)
-
-
-def validate_prefect_verion(prefect_version: str = PREFECT_VERSION):
-    # Exit if prefect version is not 2 or 3
-    if prefect_version not in ["2", "3"]:
-        print("ERROR: Prefect version must be either 2 or 3")
+    if not project_name.endswith("-dashboard"):
+        print("ERROR: Convention - project_name should end with '-dashboard'")
         sys.exit(1)
 
 
 if __name__ == "__main__":
     validate_project_name()
-    validate_prefect_verion()
